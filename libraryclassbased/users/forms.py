2cfdbb0cb3b5cd3bba973f3d916735d0ba6ec3ad
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2025-12-29 08:31:31 +0530

    Added user registartion feature to library project

---
+++ libraryclassbased/users/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username','password1','first_name','last_name','password2'] #password2-confirm password
+        
--- libraryclassbased/users/views.py
+++ libraryclassbased/users/views.py
@@ -1,7 +1,9 @@
+
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from users.forms import SignupForm
 from django.views import View
+
 
 # def register(request):
 #     return render(request,'userregister.html')
@@ -10,9 +12,21 @@ from django.views import View
 # def userlogout(request):
 #     return redirect('users:userlogin')
 
+
 class Register(View):
     def get(self,request):
-        return render(request,'userregister.html')
+        form_instance=SignupForm()
+        context={"form":form_instance}
+        return render(request,'userregister.html',context)
+    
+    def post(self,request):
+        form_instance=SignupForm(request.POST,request.FILES)
+        if form_instance.is_valid():
+            form_instance.save()
+            return redirect('users:userlogin')
+        else:
+            print(form_instance.errors)
+    
 class UserLogin(View):
     def get(self,request):
         return render(request,"userLogin.html")
