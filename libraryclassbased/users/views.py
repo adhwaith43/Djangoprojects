eb6e30060b5a2c8f259068b24f6318baa4ac4291
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2025-12-29 10:31:44 +0530

    corrected login and logout of library project

--- libraryclassbased/users/views.py
+++ libraryclassbased/users/views.py
@@ -28,8 +28,30 @@ class Register(View):
         else:
             print(form_instance.errors)
     
-
+from django.contrib.auth import authenticate,login,logout
 class Userlogin(View):
+    def post(self,request):
+        form_instance=LoginForm(request.POST)
+        if form_instance.is_valid():
+            data=form_instance.cleaned_data
+            print(data)
+            u=data['username'] #reads the username from cleaned data and assigns to variable u
+            p=data['password'] #reads the username from cleaned data and assigns to variable p
+                               #authenticate/login/logout --built in functions deafined inside contrib.auth module
+            user=authenticate(username=u,password=p)
+                   #authenticate() checks whether a user record exists in the User table
+                   #matching with the submitted username and password.if yes,returns the
+                   #user object ellse it returns none.
+
+            if user:
+                login(request,user)             #adds the user into the current session
+
+                return redirect('books:home')   #redirects to the homepage
+
+            else:
+                return HttpResponse("Invalid User credentials")
+            
+
     def get(self,request):
         form_instance=LoginForm()
         context={"form":form_instance}
@@ -37,6 +59,7 @@ class Userlogin(View):
     
 class Userlogout(View):
     def get(self,request):
+        logout(request)    #removes the current user from the session
         return redirect('users:userlogin')
 
 
