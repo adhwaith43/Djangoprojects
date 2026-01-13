d182a01b313db79f96cbf94956968f762220ce19
Adhwaith A M <131666137+adhwaith43@users.noreply.github.com>
2026-01-13 10:13:14 +0530

    corrected styles of product page added product images

--- Mainproject/ecommerce/shop/models.py
+++ Mainproject/ecommerce/shop/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     price=models.DecimalField(max_digits=10, decimal_places=2)
     stock=models.IntegerField()
     available=models.BooleanField(default=True)
-    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE,related_name="products")
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     
